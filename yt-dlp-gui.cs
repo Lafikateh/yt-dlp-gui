a23686454e8f837c2056f103c14bc14b6d907035
Lafika <lafikateh@gmail.com>
2022-09-18 22:20:22 +0200

    Second prototype

--- yt-dlp-gui.cs
+++ yt-dlp-gui.cs
@@ -27,13 +27,16 @@ namespace Lafika
 		// Internal data
 		private static bool Continue;
 		private static Form Window;
+		private static Font Regular;
+		private static Font Small;
 		private static Label LinkLabel;
 		private static TextBox Link;
 		private static Label DirectoryLabel;
 		private static TextBox Directory;
-		private static Label AudioOnlyLabel;
-		private static CheckBox AudioOnly;
+		private static Label DownloadVideoLabel;
+		private static CheckBox DownloadVideo;
 		private static Button Download;
+		private static ProgressBar Progress;
 		
 		// Window closed event function
 		private static void Window_Closed(Object Sender, FormClosedEventArgs Arguments)
@@ -47,28 +50,48 @@ namespace Lafika
 			// Enable the controls
 			Link.Enabled = true;
 			Directory.Enabled = true;
-			AudioOnly.Enabled = true;
+			DownloadVideo.Enabled = true;
 			Download.Enabled = true;
+			
+			// Stop the progress bar
+			Progress.MarqueeAnimationSpeed = 0;
 		}
 		
-		// Download button proessed event function
+		// Directory textbox pressed event function
+		private static void Directory_Pressed(Object Sender, EventArgs Arguments)
+		{
+			// Prepare the save dialog
+			SaveFileDialog Dialog = new SaveFileDialog();
+			Dialog.FileName = "Output directory";
+			
+			// Show the dialog and get the directory path
+			if(Dialog.ShowDialog() == DialogResult.OK)
+			{
+				Directory.Text = Path.GetDirectoryName(Dialog.FileName);
+			}
+		}
+		
+		// Download button pressed event function
 		private static void Download_Pressed(Object Sender, EventArgs Arguments)
 		{
 			// Disable the controls
 			Link.Enabled = false;
 			Directory.Enabled = false;
-			AudioOnly.Enabled = false;
+			DownloadVideo.Enabled = false;
 			Download.Enabled = false;
+			
+			// Start the progress bar
+			Progress.MarqueeAnimationSpeed = 1;
 			
 			// Prepare the parameters string
 			string Parameters = "--no-playlist ";
-			if(AudioOnly.Checked == true)
+			if(DownloadVideo.Checked == true)
 			{
-				Parameters = Parameters + "--extract-audio " + Link.Text;
+				Parameters = Parameters + Link.Text;
 			}
 			else
 			{
-				Parameters = Parameters + Link.Text;
+				Parameters = Parameters + "--extract-audio " + Link.Text;
 			}
 			
 			// Add the output directory to the parameters string
@@ -91,7 +114,7 @@ namespace Lafika
 		}
 		
 		// Entry point of the program
-		public static int Main()
+		[STAThread] public static int Main()
 		{
 			// Check if the yt-dlp executable is present
 			if(File.Exists("yt-dlp.exe") == false)
@@ -119,56 +142,97 @@ namespace Lafika
 			
 			// Create the window
 			Window = new Form();
-			Window.Width = 854;
-			Window.Height = 480;
+			Window.Width = 560;
+			Window.Height = 288;
 			Window.FormClosed += Window_Closed;
 			Window.Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
 			Window.Text = "GUI for yt-dlp.exe";
 			
+			// Create fonts
+			Regular = new Font("Arial", 14.0f);
+			Small = new Font("Arial", 12.0f);
+			
 			// Create the link label
 			LinkLabel = new Label();
+			LinkLabel.Top = 16;
+			LinkLabel.Left = 16;
+			LinkLabel.Width = 128;
+			LinkLabel.Height = 24;
+			LinkLabel.Font = Regular;
 			LinkLabel.Text = "Target URL:";
 			
 			// Create the link textbox
 			Link = new TextBox();
-			Link.Left = 32;
-			Link.Top = 32;
+			Link.Left = 16;
+			Link.Top = 40;
+			Link.Width = 512;
+			Link.Height = 24;
+			Link.Font = Small;
 			
 			// Create the directory label
 			DirectoryLabel = new Label();
+			DirectoryLabel.Left = 16;
+			DirectoryLabel.Top = 72;
+			DirectoryLabel.Width = 256;
+			DirectoryLabel.Height = 24;
+			DirectoryLabel.Font = Regular;
 			DirectoryLabel.Text = "Output directory:";
 			
 			// Create the directory textbox
 			Directory = new TextBox();
-			Directory.Left = 32;
-			Directory.Top = 256;
+			Directory.Left = 16;
+			Directory.Top = 96;
+			Directory.Width = 512;
+			Directory.Height = 24;
+			Directory.ReadOnly = true;
+			Directory.Click += Directory_Pressed;
+			Directory.Font = Small;
 			
-			// Create the audio only label
-			AudioOnlyLabel = new Label();
-			AudioOnlyLabel.Text = "Just audio";
+			// Create the download video label
+			DownloadVideoLabel = new Label();
+			DownloadVideoLabel.Left = 16;
+			DownloadVideoLabel.Top = 128;
+			DownloadVideoLabel.Width = 160;
+			DownloadVideoLabel.Height = 24;
+			DownloadVideoLabel.Font = Regular;
+			DownloadVideoLabel.Text = "Download video:";
 			
-			// Create the audio only checkbox
-			AudioOnly = new CheckBox();
-			AudioOnly.Left = 256;
-			AudioOnly.Top = 256;
-			AudioOnly.Width = 32;
-			AudioOnly.Height = 32;
+			// Create the download video checkbox
+			DownloadVideo = new CheckBox();
+			DownloadVideo.Left = 180;
+			DownloadVideo.Top = 124;
+			DownloadVideo.Width = 32;
+			DownloadVideo.Height = 32;
+			DownloadVideo.Checked = true;
 			
 			// Create the download button
 			Download = new Button();
-			Download.Left = 32;
-			Download.Top = 96;
+			Download.Left = 16;
+			Download.Top = 160;
+			Download.Width = 512;
+			Download.Height = 32;
+			Download.Font = Regular;
 			Download.Click += Download_Pressed;
 			Download.Text = "Download";
+			
+			// Create the progress bar
+			Progress = new ProgressBar();
+			Progress.Left = 16;
+			Progress.Top = 208;
+			Progress.Width = 512;
+			Progress.Height = 24;
+			Progress.Style = ProgressBarStyle.Marquee;
+			Progress.MarqueeAnimationSpeed = 0;
 			
 			// Add all of the elements to the window and show it
 			Window.Controls.Add(LinkLabel);
 			Window.Controls.Add(Link);
 			Window.Controls.Add(DirectoryLabel);
 			Window.Controls.Add(Directory);
-			Window.Controls.Add(AudioOnlyLabel);
-			Window.Controls.Add(AudioOnly);
+			Window.Controls.Add(DownloadVideoLabel);
+			Window.Controls.Add(DownloadVideo);
 			Window.Controls.Add(Download);
+			Window.Controls.Add(Progress);
 			Window.Show();
 			
 			// Loop forever until window is closed
